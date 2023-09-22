ee100d6a7f8303628ae644a402ce5d3eea739678
SparkySimp <info@yigitovski.com>
2023-09-22 11:19:11 +0300

    minor tweak so i can use leq instead of lt

--- bin/bmi_app.dart
+++ bin/bmi_app.dart
@@ -5,6 +5,7 @@ double computeBMI(double height, double mass) {
   return mass / (height * height);
 }
 
+double lessThanThis(double n) => n - double.minPositive;
 void main(List<String> arguments) {
   late double height, mass;
 
@@ -12,6 +13,20 @@ void main(List<String> arguments) {
   height = double.parse(stdin.readLineSync()!);
   print("Kilonuzu girin (kilogram):");
   mass = double.parse(stdin.readLineSync()!);
+  var bmi = computeBMI(height, mass);
+  print("VKİ'niz: $bmi kg/m^2");
 
-  print("VKİ'niz: ${computeBMI(height, mass)}kg/m^2");
+  if (bmi < 20) {
+    print("Zayıf");
+  } else if (bmi >= 20 && bmi <= lessThanThis(25)) {
+    print("Normal");
+  } else if (bmi >= 25 && bmi <= lessThanThis(30)) {
+    print("Hafif şişman");
+  } else if (bmi >= 30 && bmi <= lessThanThis(35)) {
+    print("Şişman");
+  } else if (bmi >= 35 && bmi <= lessThanThis(45)) {
+    print("Sağlık açısından önemli");
+  } else {
+    print("Acilen diyetisyene gitmen lazım abla");
+  }
 }
